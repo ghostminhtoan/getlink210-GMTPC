f7e688d15ca135e953fc37e27fe94d2be06545ef
GMTPC <ghostminhtoan@gmail.com>
2026-06-07 21:11:48 +0700

    ui-compact-gallery-panel

--- BuildInfo.cs
+++ BuildInfo.cs
@@ -2,6 +2,6 @@ namespace get_link_manga
 {
     public static class BuildInfo
     {
-        public const string DisplayText = "2026-06-07 ∕ 08.58.31 PM Sunday";
+        public const string DisplayText = "2026-06-07 ∕ 09.10.34 PM Sunday";
     }
 }
