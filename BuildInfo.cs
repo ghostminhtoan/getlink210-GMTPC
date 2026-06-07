9849e83e383f6a4ab3b54f2f8c0f7c3e209c5997
GMTPC <ghostminhtoan@gmail.com>
2026-06-07 13:33:44 +0700

    fix: make build info visible in xaml

--- BuildInfo.cs
+++ BuildInfo.cs
@@ -1,7 +1,7 @@
 namespace get_link_manga
 {
-    internal static class BuildInfo
+    public static class BuildInfo
     {
-        public const string DisplayText = "2026-06-07 01.12.21 PM Sunday";
+        public const string DisplayText = "2026-06-07 01.32.36 PM Sunday";
     }
 }
